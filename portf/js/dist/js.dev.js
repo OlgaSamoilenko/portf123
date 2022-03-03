78289749a796238a40e8a56cab33032ec25c9dd4
OlgaSamoilenko <70361687+OlgaSamoilenko@users.noreply.github.com>
2022-03-03 16:30:47 +0500

    Add files via upload

--- portf/js/dist/js.dev.js
+++ portf/js/dist/js.dev.js
@@ -3,3 +3,6 @@
 $(function () {
   $("#tabs").tabs();
 });
+$(document).ready(function () {
+  $("#phone").mask("+7 (999) 99-99-999");
+});
--- portf/js/js.js
+++ portf/js/js.js
@@ -3,3 +3,10 @@ $( function() {
    $( "#tabs" ).tabs();
 });
 
+
+    $(document).ready(function() {
+    $("#phone").mask("+7 (999) 99-99-999");
+  });
+
+
+
